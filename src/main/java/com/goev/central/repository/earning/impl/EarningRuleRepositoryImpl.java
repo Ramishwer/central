83048f175486f2a13c1c6b23e5b87cfa69e9651e
Ajeet Verma <ajeet@go-evmobility.com>
2024-11-08 11:35:42 +0530

    earning rule add , update, get

---
+++ src/main/java/com/goev/central/repository/earning/impl/EarningRuleRepositoryImpl.java
@@ -0,0 +1,91 @@
+package com.goev.central.repository.earning.impl;
+
+import com.goev.central.dao.earning.EarningRuleDao;
+import com.goev.central.dao.engine.EngineRuleDao;
+import com.goev.central.dao.engine.EngineRuleFieldDao;
+import com.goev.central.dto.earning.EarningRuleDto;
+import com.goev.central.repository.earning.EarningRuleRepository;
+import com.goev.central.utilities.EventExecutorUtils;
+import com.goev.central.utilities.RequestContext;
+import com.goev.record.central.tables.EarningRule;
+import com.goev.record.central.tables.records.EarningRuleRecord;
+import com.goev.record.central.tables.records.EngineRuleFieldsRecord;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.jooq.DSLContext;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import static com.goev.record.central.tables.EarningRule.EARNING_RULE;
+import static com.goev.record.central.tables.EngineRuleFields.ENGINE_RULE_FIELDS;
+import static com.goev.record.central.tables.EngineRules.ENGINE_RULES;
+
+@Slf4j
+@Repository
+@AllArgsConstructor
+public class EarningRuleRepositoryImpl implements EarningRuleRepository {
+    private final DSLContext context;
+    private final EventExecutorUtils eventExecutor;
+
+    @Override
+    public EarningRuleDao save(EarningRuleDao earningRuleDao){
+        EarningRuleRecord earningRuleRecord = context.newRecord(EARNING_RULE, earningRuleDao);
+        earningRuleRecord.setRuleId(earningRuleDao.getRuleId());
+        earningRuleRecord.store();
+        earningRuleDao.setUuid(earningRuleRecord.getUuid());
+        earningRuleDao.setRuleId(earningRuleRecord.getRuleId());
+        earningRuleDao.setCity(earningRuleRecord.getCity());
+        earningRuleDao.setBusinessType(earningRuleRecord.getBusinessType());
+        earningRuleDao.setClientName(earningRuleRecord.getClientName());
+        earningRuleDao.setFixedIncome(earningRuleRecord.getFixedIncome());
+        earningRuleDao.setVariableIncome(earningRuleRecord.getVariableIncome());
+        earningRuleDao.setChecks(earningRuleRecord.getChecks());
+        earningRuleDao.setCheckValue(earningRuleRecord.getCheckValue());
+        earningRuleDao.setValidTill(earningRuleRecord.getValidTill());
+        earningRuleDao.setIsActive(earningRuleRecord.getIsActive());
+        earningRuleDao.setCreatedOn(earningRuleRecord.getCreatedOn());
+        earningRuleDao.setCreatedBy(earningRuleRecord.getCreatedBy());
+        return earningRuleDao;
+    }
+
+    @Override
+    public EarningRuleDao update(EarningRuleDao newEarningRuleDao){
+        EarningRuleRecord earningRuleRecord = context.newRecord(EARNING_RULE, newEarningRuleDao);
+        earningRuleRecord.update();
+        newEarningRuleDao.setUuid(earningRuleRecord.getUuid());
+        newEarningRuleDao.setRuleId(earningRuleRecord.getRuleId());
+        newEarningRuleDao.setCity(earningRuleRecord.getCity());
+        newEarningRuleDao.setBusinessType(earningRuleRecord.getBusinessType());
+        newEarningRuleDao.setClientName(earningRuleRecord.getClientName());
+        newEarningRuleDao.setFixedIncome(earningRuleRecord.getFixedIncome());
+        newEarningRuleDao.setVariableIncome(earningRuleRecord.getVariableIncome());
+        newEarningRuleDao.setChecks(earningRuleRecord.getChecks());
+        newEarningRuleDao.setCheckValue(earningRuleRecord.getCheckValue());
+        newEarningRuleDao.setValidTill(earningRuleRecord.getValidTill());
+        newEarningRuleDao.setIsActive(earningRuleRecord.getIsActive());
+        newEarningRuleDao.setCreatedOn(earningRuleRecord.getCreatedOn());
+        newEarningRuleDao.setCreatedBy(earningRuleRecord.getCreatedBy());
+        return newEarningRuleDao;
+    }
+
+    @Override
+    public EarningRuleDao findByUUID(String uuid){
+        return context.selectFrom(EARNING_RULE).where(EARNING_RULE.UUID.eq(uuid))
+                .fetchAnyInto(EarningRuleDao.class);
+
+    }
+    @Override
+    public Optional<EarningRuleDao> findLastRuleIdInEarningRule (){
+        return context.selectFrom(EARNING_RULE)
+                .orderBy(EARNING_RULE.ID.desc())
+                .limit(1)
+                .fetchOptionalInto(EarningRuleDao.class);
+    }
+
+    @Override
+    public List<EarningRuleDao> findAll(){
+        return context.selectFrom(EARNING_RULE).fetchInto(EarningRuleDao.class);
+    }
+}
---
+++ src/main/java/com/goev/central/service/earning/impl/EarningRuleServiceImpl.java
@@ -0,0 +1,108 @@
+package com.goev.central.service.earning.impl;
+
+import com.goev.central.dao.earning.EarningRuleDao;
+import com.goev.central.dto.common.PaginatedResponseDto;
+import com.goev.central.dto.earning.EarningRuleDto;
+import com.goev.central.repository.earning.EarningRuleRepository;
+import com.goev.central.service.earning.EarningRuleService;
+import com.goev.lib.exceptions.ResponseException;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
+
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class EarningRuleServiceImpl implements EarningRuleService {
+    private final EarningRuleRepository earningRuleRepository;
+
+
+    @Override
+    public PaginatedResponseDto<EarningRuleDto> getEarningRules(){
+        PaginatedResponseDto<EarningRuleDto> result = PaginatedResponseDto.<EarningRuleDto>builder().elements(new ArrayList<>()).build();
+        List<EarningRuleDao> earningRuleDaos = earningRuleRepository.findAll();
+        if (CollectionUtils.isEmpty(earningRuleDaos))
+            return result;
+
+        for (EarningRuleDao engineRuleDao :earningRuleDaos ) {
+            result.getElements().add(EarningRuleDto.fromDao(engineRuleDao));
+        }
+        return result;
+    }
+
+    @Override
+    public EarningRuleDto createEarningRule(EarningRuleDto earningRuleDto) {
+        EarningRuleDao earningRuleDao = new EarningRuleDao();
+        earningRuleDao.setRuleId(generateNextSerialNumber());
+        earningRuleDao.setCity(earningRuleDto.getCity());
+        earningRuleDao.setBusinessType(earningRuleDto.getBusinessType());
+        earningRuleDao.setClientName(earningRuleDto.getClientName());
+        earningRuleDao.setFixedIncome(earningRuleDto.getFixedIncome());
+        earningRuleDao.setVariableIncome(earningRuleDto.getVariableIncome());
+        earningRuleDao.setChecks(earningRuleDto.getChecks());
+        earningRuleDao.setCheckValue(earningRuleDto.getCheckValue());
+        earningRuleDao.setValidTill(earningRuleDto.getValidTill());
+        earningRuleDao.setIsActive(earningRuleDto.getIsActive());
+        earningRuleDao.setCreatedOn(earningRuleDto.getCreatedOn());
+        earningRuleDao.setCreatedBy(earningRuleDto.getCreatedBy());
+        earningRuleDao=earningRuleRepository.save(earningRuleDao);
+        if (earningRuleDao == null) {
+            throw new ResponseException("Error in saving EarningRule");
+        }
+        return EarningRuleDto.fromDao(earningRuleDao);
+    }
+
+    @Override
+    public EarningRuleDto updateEarningRule (EarningRuleDto earningRuleDto , String earningRuleUUID){
+        EarningRuleDao earningRuleDao = earningRuleRepository.findByUUID(earningRuleUUID);
+        if (earningRuleDao == null)
+            throw new ResponseException("No EarningRule  found for Id :" + earningRuleUUID);
+
+        EarningRuleDao newEarningRuleDao = new EarningRuleDao();
+        newEarningRuleDao.setId(earningRuleDao.getId());
+        newEarningRuleDao.setUuid(earningRuleDao.getUuid());
+        newEarningRuleDao.setRuleId(earningRuleDao.getRuleId());
+        newEarningRuleDao.setCreatedBy(earningRuleDao.getCreatedBy());
+
+        newEarningRuleDao.setCity(earningRuleDto.getCity());
+        newEarningRuleDao.setBusinessType(earningRuleDto.getBusinessType());
+        newEarningRuleDao.setClientName(earningRuleDto.getClientName());
+        newEarningRuleDao.setFixedIncome(earningRuleDto.getFixedIncome());
+        newEarningRuleDao.setVariableIncome(earningRuleDto.getVariableIncome());
+        newEarningRuleDao.setChecks(earningRuleDto.getChecks());
+        newEarningRuleDao.setCheckValue(earningRuleDto.getCheckValue());
+        newEarningRuleDao.setValidTill(earningRuleDto.getValidTill());
+        newEarningRuleDao.setIsActive(earningRuleDto.getIsActive());
+        newEarningRuleDao.setUpdatedOn(earningRuleDto.getUpdatedOn());
+        newEarningRuleDao.setUpdatedBy(earningRuleDto.getUpdatedBy());
+        earningRuleDao = earningRuleRepository.update(newEarningRuleDao);
+
+        if (earningRuleDao == null)
+            throw new ResponseException("Error in updating details EarningRule");
+        return EarningRuleDto.fromDao(earningRuleDao);
+    }
+
+
+    public String generateNextSerialNumber() {
+        Optional<EarningRuleDao> latestRule = earningRuleRepository.findLastRuleIdInEarningRule();
+
+        String newSerialNumber;
+        if (latestRule.isPresent()) {
+            String lastSerial = latestRule.get().getRuleId();
+            int lastNumber = Integer.parseInt(lastSerial.substring(2));
+            int nextNumber = lastNumber + 1;
+            DecimalFormat formatter = new DecimalFormat("RU000");
+            newSerialNumber = formatter.format(nextNumber);
+        } else {
+            newSerialNumber = "RU001";
+        }
+        return newSerialNumber;
+    }
+
+}
